f4eedfcdd6b5a1e64824e2a7b9ce41dde9db13c0
Sebastian <sebas.mq10@gmail.com>
2025-11-28 11:27:18 -0300

    now the header is fixed and sticky

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,8 +5,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="absolute top-0 bg-red-500 flex justify-center w-full py-4">
-
+    <header className="fixed z-50 top-0 left-0 right-0 bg-red-500 flex justify-center w-full py-4">
       <div className="flex items-center gap-8">
         {/* Logo Section */}
         <Link href="/">
@@ -33,7 +32,7 @@ export default function Header() {
             About Us
           </Link>
           <Link
-            className="text-text-primary/70 text-sm font-medium hover:text-gray-200 transition-colors"
+            className="text-sm font-medium hover:text-gray-200 transition-colors"
             href="#"
           >
             Contact
