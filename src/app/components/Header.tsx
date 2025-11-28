e2396a85aa3cbfca8e7d28dbc0838a9a32172f7e
Sebastian <sebas.mq10@gmail.com>
2025-11-28 13:58:16 -0300

    now the logo is in the right place, header looks good and the scroll is smooth. Sample HTML was also added to page.tsx to test smooth scrolling,links, etc

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,45 +1,30 @@
 'use client';
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
-
 
 export default function Header() {
   return (
-    <header className="fixed z-50 top-0 left-0 right-0 bg-red-500 flex justify-center w-full py-4">
-      <div className="flex items-center gap-8">
-        {/* Logo Section */}
-        <Link href="/">
-          <span className="text-xl font-bold">Cama Sushi</span>
-        </Link>
-        {/* Desktop Navigation */}
-        <nav className="items-center gap-6 md:flex">
-          <Link
-            className="text-sm font-medium hover:text-gray-200 transition-colors"
-            href="/"
-          >
-            Home
-          </Link>
-          <Link
-            className="text-sm font-medium hover:text-gray-200 transition-colors"
-            href="#"
-          >
-            Menu
-          </Link>
-          <Link
-            className="text-sm font-medium hover:text-gray-200 transition-colors"
-            href="#"
-          >
-            About Us
-          </Link>
-          <Link
-            className="text-sm font-medium hover:text-gray-200 transition-colors"
-            href="#"
-          >
-            Contact
-          </Link>
+    <header className="fixed z-50 top-0 left-0 right-0 w-full bg-white text-black shadow-sm py-3">
+      <div className="relative w-full flex items-center">
+        <div className="pl-0 ml-4 relative h-20 w-25">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            fill
+            sizes="1"
+            className="object-cover rounded-full"
+            loading="eager"
+          />
+        </div>
+        <nav className="hidden md:flex absolute left-1/2 -translate-x-1/2 items-center gap-8">
+          <Link className="text-lg font-medium hover:text-gray-400 transition-colors" href="/">Home</Link>
+          <Link className="text-md font-medium hover:text-gray-400 transition-colors" href="#menu">Menu</Link>
+          <Link className="text-md font-medium hover:text-gray-400 transition-colors" href="#about">About Us</Link>
+          <Link className="text-md font-medium hover:text-gray-400 transition-colors" href="#contact">Contact</Link>
         </nav>
+
       </div>
     </header>
   );
-};
-
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,29 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>
-          <Header />
-        </div>
-      </main >
-    </div >
+    <div className="min-h-screen w-full bg-zinc-50 font-sans dark:bg-black">
+      <Header />
+
+      {/* Contenido */}
+      <main className="pt-32">
+
+        <section id="home" className="min-h-[120vh] bg-red-100 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-black">Home Section</h1>
+        </section>
+
+        <section id="menu" className="min-h-[120vh] bg-green-100 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-black">Menu Section</h1>
+        </section>
+
+        <section id="about" className="min-h-[120vh] bg-blue-100 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-black">About Us Section</h1>
+        </section>
+
+        <section id="contact" className="min-h-[120vh] bg-yellow-100 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-black">Contact Section</h1>
+        </section>
+
+      </main>
+    </div>
   );
 }
