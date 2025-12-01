520f0421f9ff198fd369c1bd3c4e9e962827458f
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-01 15:51:15 -0300

    unit-blue

--- src/features/menu/components/SushiCard.test.tsx
+++ src/features/menu/components/SushiCard.test.tsx
@@ -1,21 +1,9 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SushiCard from '@/features/menu/components/SushiCard';
 
-test("SushiCard renders the sushi name", () => {
-  const mockSushi = {
-    id: 1,
-    name: "Roll de Pacú",
-    description: "description text",
-    is_fried: false,
-    type: "Roll",
-    price: 1000
-  }
-  render(<SushiCard sushi={mockSushi} />)
-  expect(screen.getByText(/roll de pacú/i)).toBeInTheDocument();
-});
+describe("SushiCard", () => {
 
-test("SushiCard redners the sushi ingredients", () => {
   const mockSushi = {
     id: 1,
     name: "Roll de Pacú",
@@ -23,7 +11,18 @@ test("SushiCard redners the sushi ingredients", () => {
     is_fried: false,
     type: "Roll",
     price: 1000
-  }
-  render(<SushiCard sushi={mockSushi} />)
-  expect(screen.getByText(/description text/i)).toBeInTheDocument();
-});
+  };
+
+  test("It renders the sushi name", () => {
+    render(<SushiCard sushi={mockSushi} />)
+    expect(screen.getByText(/roll de pacú/i)).toBeInTheDocument();
+  });
+
+  test("It renders the sushi ingredients", () => {
+    render(<SushiCard sushi={mockSushi} />)
+    expect(screen.getByText(/description text/i)).toBeInTheDocument();
+  });
+})
+
+
+
