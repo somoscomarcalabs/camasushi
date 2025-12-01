011175dafad12ef76d18e185ad957955616d0bfe
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-01 15:39:26 -0300

    unit-red: Sushi Card renders the sushi name

--- src/features/menu/components/SushiCard.test.tsx
+++ src/features/menu/components/SushiCard.test.tsx
@@ -1,1 +1,16 @@
+import { test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import SushiCard from '@/features/menu/components/SushiCard';
 
+test("SushiCard renders the sushi name", () => {
+  const mockSushi = {
+    id: 1,
+    name: "Roll de Pacú",
+    description: "description text",
+    is_fried: false,
+    type: "Roll",
+    price: 1000
+  }
+  render(<SushiCard sushi={mockSushi} />)
+  expect(screen.getByText(/roll de pacú/i)).toBeInTheDocument();
+});
---
+++ src/features/menu/components/SushiCard.tsx
@@ -0,0 +1,5 @@
+export default function SushiCard() {
+  return (
+    <></>
+  )
+}
