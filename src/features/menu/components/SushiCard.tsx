e5f8506f3bf1731b147f61ec9138a61c43868239
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-01 15:45:32 -0300

    unit-blue

--- src/features/menu/components/SushiCard.tsx
+++ src/features/menu/components/SushiCard.tsx
@@ -1,4 +1,15 @@
-export default function SushiCard({ sushi }) {
+interface SushiProps {
+  sushi: {
+    id: number,
+    name: string,
+    description: string,
+    is_fried: boolean,
+    type: string,
+    price: number
+  }
+}
+
+export default function SushiCard({ sushi }: SushiProps) {
   return (
     <div>
       <h3>{sushi.name}</h3>
