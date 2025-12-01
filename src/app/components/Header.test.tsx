60fea465908941bef4522f92626ad574541cebdb
Sebastian <sebas.mq10@gmail.com>
2025-12-01 16:18:32 -0300

    test:unit-b

--- src/app/components/Header.test.tsx
+++ src/app/components/Header.test.tsx
@@ -1,14 +1,18 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react'
 import Header from './Header';
 
 
-test('The Header component renders correctly', () => {
-  expect(() => render(<Header />)).not.toThrow();
-})
+describe("The Header Component...", () => {
 
-test("renders the logo", () => {
-  render(<Header />);
-  const logo = screen.getByAltText("Logo");
-  expect(logo).toBeInTheDocument();
+  test('...renders correctly', () => {
+    expect(() => render(<Header />)).not.toThrow();
+  })
+
+  test("...renders the logo", () => {
+    render(<Header />);
+    const logo = screen.getByAltText("Logo");
+    expect(logo).toBeInTheDocument();
+  })
+
 })
