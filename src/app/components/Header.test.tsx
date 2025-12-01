06364d93214c703f5cbf93dcbb8fe4ca28df1468
Sebastian <sebas.mq10@gmail.com>
2025-12-01 16:14:06 -0300

    test - renders the logo - green

--- src/app/components/Header.test.tsx
+++ src/app/components/Header.test.tsx
@@ -1,8 +1,14 @@
 import { test, expect } from 'vitest';
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Header from './Header';
 
 
 test('The Header component renders correctly', () => {
   expect(() => render(<Header />)).not.toThrow();
+})
+
+test("renders the logo", () => {
+  render(<Header />);
+  const logo = screen.getByAltText("Logo");
+  expect(logo).toBeInTheDocument();
 })
