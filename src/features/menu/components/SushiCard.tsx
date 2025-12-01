6eebb44ba7a4de1d69a665cadc4294ce213232ce
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-01 17:21:50 -0300

    unit-green

--- src/features/menu/components/SushiCard.tsx
+++ src/features/menu/components/SushiCard.tsx
@@ -4,11 +4,11 @@ interface SushiProps {
   sushi: {
     id: number,
     name: string,
-    description: string,
+    ingredients: string[],
     is_fried: boolean,
-    type: string,
     price: number,
-    image: string
+    image: string,
+    pieces: number
   }
 }
 
@@ -17,10 +17,11 @@ export default function SushiCard({ sushi }: SushiProps) {
     <div>
       <Image src={sushi.image} alt={sushi.name} width={300} height={200} />
       <h3>{sushi.name}</h3>
-      <p>{sushi.description}</p>
+      <p>{sushi.ingredients.join(', ')}</p>
       <span>${sushi.price.toLocaleString('es-AR')}</span>
+      <span>{sushi.pieces === 1 ? "1 Unidad" : `${sushi.pieces} unidades`}</span>
       {sushi.is_fried && (
-        <span>Rebozado</span>
+        <span>Frito</span>
       )}
     </div>
   )
