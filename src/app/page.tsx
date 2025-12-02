366b8acf452db9e62cb311ddedbdbb6ee6b3d54b
Sebastian <sebas.mq10@gmail.com>
2025-12-02 13:39:26 -0300

    footer presented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import Image from "next/image";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+    <div>
+      <main>
+        <section
+          id="home"
+          className="min-h-screen flex items-center justify-center bg-red-200 px-6 py-24"
+        >
+          <h1 className="text-5xl font-bold text-gray-900">Home Section</h1>
+        </section>
+        <section
+          id="menu"
+          className="min-h-screen flex items-center justify-center bg-green-200 px-6 py-24"
+        >
+          <h1 className="text-5xl font-bold text-gray-900">Menu Section</h1>
+        </section>
+        <section
+          id="about"
+          className="min-h-screen flex items-center justify-center bg-blue-200 px-6 py-24"
+        >
+          <h1 className="text-5xl font-bold text-gray-900">About Us Section</h1>
+        </section>
+        <section
+          id="contact"
+          className="min-h-screen flex items-center justify-center bg-yellow-200 px-6 py-24"
+        >
+          <h1 className="text-5xl font-bold text-gray-900">Contact Section</h1>
+        </section>
+        <Footer />
       </main >
     </div >
   );
