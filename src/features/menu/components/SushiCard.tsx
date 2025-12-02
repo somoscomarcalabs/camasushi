f86e6c93a0364a6db72859023f1b6c225798264b
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-02 12:17:49 -0300

    styles added

--- src/features/menu/components/SushiCard.tsx
+++ src/features/menu/components/SushiCard.tsx
@@ -14,9 +14,9 @@ interface SushiProps {
 
 export default function SushiCard({ sushi }: SushiProps) {
   return (
-    <div className='w-full max-w-sm rounded-2xl overflow-hidden shadow-lg text-foreground dark:text-muted dark:bg-accent'>
+    <div className=' group w-full max-w-sm rounded-2xl overflow-hidden shadow-lg text-foreground dark:text-muted dark:bg-accent transition-all duration-300 hover:shadow-2xl hover:-translate-y-1'>
       <div className='relative h-48 w-full'>
-        <Image src={sushi.image} alt={sushi.name} fill className='object-cover' />
+        <Image src={sushi.image} alt={sushi.name} fill className='object-cover transition-transform duration-500 group-hover:scale-110' />
         {sushi.is_fried && (
           <span className='absolute top-3 left-3 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded shadow-md uppercase tracking-wide'>Frito</span>
         )}
@@ -25,10 +25,10 @@ export default function SushiCard({ sushi }: SushiProps) {
         <h3 className="text-xl mb-2">{sushi.name}</h3>
         <p className="text-sm mb-6 leading-relaxed">{sushi.ingredients.join(', ')}</p>
         <div className="flex items-center justify-between">
-          <span>${sushi.price.toLocaleString('es-AR')}</span>
+          <span className="text-lg md:text-lg font-bold">${sushi.price.toLocaleString('es-AR')}</span>
           <span>{sushi.pieces === 1 ? "1 Unidad" : `${sushi.pieces} unidades`}</span>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
