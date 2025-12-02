c382de1437c9fcd04c78bd2e4a4faa6843d7ceb8
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-02 15:11:22 -0300

    unit-green

--- src/features/landing/components/Hero.tsx
+++ src/features/landing/components/Hero.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <section data-testid="hero-section" className="container mx-auto px-4 py-12 text-center text-secondary">
-      <h1 className="text-4xl md:text-6xl mb-2">El mejor Sushi de Puerto Rico</h1>
-      <p className="uppercase mb-4">Sushi hecho a mano con ingredientes frescos.</p>
-      <a href="#menu" className="inline-block uppercase bg-accent text-white font-bold py-3 px-8 rounded-full hover: transition duration-300">Mirá nuestro Menú</a>
+      <div>
+        <h1 className="text-4xl md:text-6xl mb-2">El mejor Sushi de Puerto Rico</h1>
+        <p className="uppercase mb-4">Sushi hecho a mano con ingredientes frescos.</p>
+        <a href="#menu" className="inline-block uppercase bg-accent text-white font-bold py-3 px-8 rounded-full hover: transition duration-300">Mirá nuestro Menú</a>
+      </div>
+      <div>
+        <Image src="/images/ui/LogoCama_Sushi.webp" width={300} height={200} alt="camasushi-logo" className="" />
+      </div>
     </section>
   )
 }
