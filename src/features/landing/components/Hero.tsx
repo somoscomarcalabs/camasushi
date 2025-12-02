43680bcde6defb046f6ef3b110a744904ca5205f
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-02 13:11:15 -0300

    unit-green

--- src/features/landing/components/Hero.tsx
+++ src/features/landing/components/Hero.tsx
@@ -1,5 +1,7 @@
 export default function Hero() {
   return (
-    <section data-testid="hero-section"></section>
+    <section data-testid="hero-section">
+      <h1>El mejor Sushi de Puerto Rico</h1>
+    </section>
   )
 }
