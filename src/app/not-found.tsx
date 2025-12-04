02bc1d5e0b98ef8a09ca7e893a66bcf2c10a89f2
Matias <matias.acastro1@gmail.com>
2025-12-04 17:25:16 -0300

    unit:green

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,6 @@
 export default function NotFound() {
-  return <></>
+  console.log('se renderizó la página 404')
+  return <div data-testid='not-found-page'>
+    <h1>Hola Error 404</h1>
+  </div>
 }
