eb8486536d135e41f209e3f825d5d0c40836d2b6
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-04 18:15:51 -0300

    unit-red: Menu renders a SushiCard for each item

--- src/features/landing/components/Menu.test.tsx
+++ src/features/landing/components/Menu.test.tsx
@@ -1,6 +1,14 @@
-import { test, expect, describe } from 'vitest';
+import { test, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Menu from '@/features/landing/components/Menu';
+
+vi.mock('@/features/menu/components/SushiCard', () => ({
+  default: (props: any) => (
+    <div data-testid="mock-sushi-card">
+      {props.sushi.name}
+    </div>
+  )
+}));
 
 describe("Menu component", () => {
 
@@ -11,13 +19,33 @@ describe("Menu component", () => {
 
   test("Renders the correct number of items", () => {
     const mockItems = [
-      { id: 1, name: "Pacú" },
-      { id: 2, name: "Salmón" }
+      { name: "Pacú" },
+      { name: "Atún" }
     ]
     render(<Menu items={mockItems} />);
     expect(screen.getAllByTestId("menu-item")).toHaveLength(2);
   });
 
+});
+
+test("Menu renders a SushiCard for each item", () => {
+  const mockItems = [
+    {
+      id: 1,
+      name: "Pacú",
+      ingredients: ["Pacú", "queso"],
+      description: "Una pieza...",
+      is_fried: true,
+      price: 5300,
+      image: "/images/pacu.jpg",
+      pieces: 5
+    },
+  ]
+  render(<Menu items={mockItems} />)
+  const e = screen.getByTestId("mock-sushi-card")
+  console.log({ e: e.innerHTML })
+  expect(screen.getByTestId("mock-sushi-card")).toBeInTheDocument();
+  expect(screen.getByTestId("mock-sushi-card")).toHaveTextContent("Pacú")
 });
 
 
