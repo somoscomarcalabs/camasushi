884a53b794e409a5e8b42e8748898e56e7dd1889
Matias <matias.acastro1@gmail.com>
2025-12-04 17:28:53 -0300

    unit:blue

--- src/app/not-found.test.tsx
+++ src/app/not-found.test.tsx
@@ -1,9 +1,9 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 import NotFound from '@/app/not-found';
 
 test('it can render without errors', () => {
-  render(<NotFound />)
-  expect(screen.getByTestId('not-found-page')).toBeInTheDocument()
+  render(<NotFound />);
+  expect(screen.getByTestId('not-found-page')).toBeInTheDocument();
 })
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,4 @@
 export default function NotFound() {
-  console.log('se renderizó la página 404')
   return <div data-testid='not-found-page'>
-    <h1>Hola Error 404</h1>
   </div>
 }
