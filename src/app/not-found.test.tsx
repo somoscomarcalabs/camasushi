904691d0c52f457f908af874ad1c8e083556b8c4
Matias <matias.acastro1@gmail.com>
2025-12-04 17:18:09 -0300

    unit:red

---
+++ src/app/not-found.test.tsx
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import NotFound from '@/app/not-found';
+
+test('it can render without errors', () => {
+  render(<NotFound />)
+  expect(screen.getByTestId('not-found-page')).toBeInTheDocument()
+})
