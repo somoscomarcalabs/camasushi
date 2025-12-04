33e1b09b3d0e9a4004a3c961b57882b24269fbed
Sebastian <sebas.mq10@gmail.com>
2025-12-04 17:30:16 -0300

    button redirects to whatsapp

--- src/features/contact/contact.tsx
+++ src/features/contact/contact.tsx
@@ -12,3 +12,6 @@ const instagramUsername = "camasushi";
 
 //URL de Instagram
 export const instagramUrl = `https://instagram.com/${instagramUsername}`;
+
+//URL de Maps
+export const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d890.2257410108905!2d-55.02331893046934!3d-26.81121820161122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8271970e60731%3A0xc705252c52c4bdb8!2sJos%C3%A9%20Manuel%20Estrada%20206%2C%20N3334%20Puerto%20Rico%2C%20Misiones!5e0!3m2!1ses!2sar!4v1764876846280!5m2!1ses!2sar";
--- src/features/landing/components/About.tsx
+++ src/features/landing/components/About.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import Image from 'next/image';
+import { whatsappUrl, mapUrl } from '@/features/contact/contact';
 
 export default function About() {
-  const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d890.2257410108905!2d-55.02331893046934!3d-26.81121820161122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8271970e60731%3A0xc705252c52c4bdb8!2sJos%C3%A9%20Manuel%20Estrada%20206%2C%20N3334%20Puerto%20Rico%2C%20Misiones!5e0!3m2!1ses!2sar!4v1764876846280!5m2!1ses!2sar";
-
   return (
     <>
       {/* Sección About - Ocupa toda la pantalla */}
@@ -123,7 +122,7 @@ export default function About() {
                 {/* Botón de acción centrado */}
                 <div className="pt-4 text-center md:text-left">
                   <a
-                    href="#contact"
+                    href={whatsappUrl}
                     className="inline-flex items-center gap-2 px-8 py-3 bg-primary dark:bg-accent text-white font-satoshi font-medium rounded-lg hover:bg-primary/90 dark:hover:bg-accent/90 transition-colors shadow-md hover:shadow-lg transform hover:-translate-y-0.5"
                   >
                     <span>Hacer un pedido</span>
