ba7cdd77bca09e2ec3ee0c5c342eaa5a6443d093
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-04 16:58:03 -0300

    unit-blue

--- src/features/landing/components/Menu.test.tsx
+++ src/features/landing/components/Menu.test.tsx
@@ -1,17 +1,23 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Menu from '@/features/landing/components/Menu';
 
-test("Menu component renders the menu container", () => {
-  render(<Menu items={[]} />);
-  expect(screen.getByTestId("menu-container")).toBeInTheDocument();
+describe("Menu component", () => {
+
+  test("Renders the menu container", () => {
+    render(<Menu items={[]} />);
+    expect(screen.getByTestId("menu-container")).toBeInTheDocument();
+  });
+
+  test("Renders the correct number of items", () => {
+    const mockItems = [
+      { id: 1, name: "Pacú" },
+      { id: 2, name: "Salmón" }
+    ]
+    render(<Menu items={mockItems} />);
+    expect(screen.getAllByTestId("menu-item")).toHaveLength(2);
+  });
+
 });
 
-test("Menu renders the correct number of items", () => {
-  const mockItems = [
-    { id: 1, name: "Pacú" },
-    { id: 2, name: "Salmón" }
-  ]
-  render(<Menu items={mockItems} />);
-  expect(screen.getAllByTestId("menu-item")).toHaveLength(2);
-});
+
