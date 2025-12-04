82e70205dd6d68c685fd961eeb0e01642d281aa3
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-04 16:49:12 -0300

    unit-red: Menu renders the correct number of items

--- src/features/landing/components/Menu.test.tsx
+++ src/features/landing/components/Menu.test.tsx
@@ -3,6 +3,15 @@ import { render, screen } from '@testing-library/react';
 import Menu from '@/features/landing/components/Menu';
 
 test("Menu component renders the menu container", () => {
-  render(<Menu />);
+  render(<Menu items={[]} />);
   expect(screen.getByTestId("menu-container")).toBeInTheDocument();
+});
+
+test("Menu renders the correct number of items", () => {
+  const mockItems = [
+    { id: 1, name: "Pacú" },
+    { id: 2, name: "Salmón" }
+  ]
+  render(<Menu items={mockItems} />);
+  expect(screen.getAllByTestId("menu-item")).toHaveLength(2);
 });
