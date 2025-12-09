a4d4c4acf85cf02416597a652b32a9c53a704f04
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-09 12:44:46 -0300

    unit-red&&green

--- src/features/landing/components/Menu.test.tsx
+++ src/features/landing/components/Menu.test.tsx
@@ -1,7 +1,7 @@
 import { test, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Menu from '@/features/landing/components/Menu';
-import { SushiItem } from '@/types/Menu';
+import { SushiItem, MenuCategory } from '@/types/Menu';
 
 vi.mock('@/features/menu/components/SushiCard', () => ({
   default: ({ sushi }: { sushi: SushiItem }) => (
@@ -13,36 +13,53 @@ vi.mock('@/features/menu/components/SushiCard', () => ({
 
 describe("Menu component", () => {
 
+  const mockCategories: MenuCategory[] = [
+    {
+      name: "Pacú",
+      items: [
+        {
+          id: 1,
+          name: "Roll de Pacú",
+          ingredients: ["Pacú", "queso"],
+          description: "Description",
+          is_fried: true,
+          price: 5000,
+          image: "/img.jpg",
+          pieces: 5
+        }
+      ]
+    },
+    {
+      name: "Langostinos",
+      items: [
+        {
+          id: 2,
+          name: "Atun",
+          ingredients: ["Atún", "queso"],
+          description: "Description",
+          is_fried: false,
+          price: 1000,
+          image: "/img.jpg",
+          pieces: 5
+
+        }
+      ]
+    }
+  ]
+
   test("Renders the menu container", () => {
-    render(<Menu items={[]} />);
+    render(<Menu categories={[]} />);
     expect(screen.getByTestId("menu-container")).toBeInTheDocument();
   });
 
   test("Renders the correct number of items", () => {
-    const mockItems = [
-      { name: "Pacú" },
-      { name: "Atún" }
-    ] as SushiItem[];
-
-    render(<Menu items={mockItems} />);
+    render(<Menu categories={mockCategories} />);
     expect(screen.getAllByTestId("menu-item")).toHaveLength(2);
   });
 
   test("Renders a SushiCard for each item", () => {
-    const mockItems: SushiItem[] = [
-      {
-        id: 1,
-        name: "Pacú",
-        ingredients: ["Pacú", "queso"],
-        description: "Una pieza...",
-        is_fried: true,
-        price: 5300,
-        image: "/images/pacu.jpg",
-        pieces: 5
-      },
-    ]
-    render(<Menu items={mockItems} />)
-    expect(screen.getByTestId("mock-sushi-card")).toBeInTheDocument();
-    expect(screen.getByTestId("mock-sushi-card")).toHaveTextContent("Pacú");
+    render(<Menu categories={mockCategories} />)
+    expect(screen.getAllByTestId("mock-sushi-card")).toHaveLength(2);
+    expect(screen.getAllByTestId("mock-sushi-card")[0]).toHaveTextContent("Pacú");
   });
 });
--- src/features/landing/components/Menu.tsx
+++ src/features/landing/components/Menu.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import SushiCard from "@/features/menu/components/SushiCard"
-import { SushiItem } from '@/types/Menu';
+import { MenuCategory } from '@/types/Menu';
 
 interface MenuProps {
-  items: SushiItem[]
+  categories: MenuCategory[]
 }
 
-export default function Menu({ items }: MenuProps) {
+export default function Menu({ categories }: MenuProps) {
   return (
     <div data-testid="menu-container">
-      {items.map((item) => (
-        <div data-testid="menu-item" key={item.id}>
-          <SushiCard sushi={item} />
-        </div>
-      ))}
+      {
+        categories.map((category) => (
+          <div key={category.name}>
+            {category.items.map((item) => (
+              <div data-testid="menu-item" key={item.id}>
+                <SushiCard sushi={item} />
+              </div>
+            ))}
+          </div>
+        ))
+      }
     </div>
   )
 }
