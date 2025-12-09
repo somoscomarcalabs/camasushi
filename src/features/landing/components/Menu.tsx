71c1c33be71a932c239b843637ac1c937eb2888c
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-12-09 13:10:12 -0300

    unit-green

--- src/features/landing/components/Menu.tsx
+++ src/features/landing/components/Menu.tsx
@@ -8,10 +8,10 @@ interface MenuProps {
 
 export default function Menu({ categories }: MenuProps) {
   return (
-    <div data-testid="menu-container" className='space-y-10'>
+    <div data-testid="menu-container" className='space-y-10 grid grid-col-1 md:grid-cols-2 gap-8'>
       {
         categories.map((category) => (
-          <div key={category.name}>
+          <div key={category.name} className=''>
             {category.items.map((item) => (
               <div data-testid="menu-item" key={item.id}>
                 <SushiCard sushi={item} />
