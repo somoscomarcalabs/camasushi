69bb4f9bd60bf5110a425d6ee563d42e2c532214
Matias <matias.acastro1@gmail.com>
2025-12-09 12:55:36 -0300

    wip: unit tests list

--- src/app/not-found.test.tsx
+++ src/app/not-found.test.tsx
@@ -7,3 +7,7 @@ test('it can render without errors', () => {
   render(<NotFound />);
   expect(screen.getByTestId('not-found-page')).toBeInTheDocument();
 })
+
+test.todo('it has a 404 image')
+test.todo('it has a title with "Página no encontrada" message')
+test.todo('it has a link to go back to the homepage')
